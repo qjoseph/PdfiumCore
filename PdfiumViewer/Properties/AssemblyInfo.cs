0d724ac0bcf45deb0fb69212ff8c4b1e23ce77ad
Enrico Piccini <epiccini@onit.it>
2021-08-02 17:34:55 +0200

    Cleaned up project

--- PdfiumViewer/Properties/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿using System;
-using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: ComVisible(false)]
-[assembly: CLSCompliant(true)]
-[assembly: Guid("ca63710b-8bc7-4fc5-8c39-210bacf591e8")]
